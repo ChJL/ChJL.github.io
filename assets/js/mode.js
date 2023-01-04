88a9322ac71d1ee9917c06066e265056587903f9
ChJL <linjaak@gmail.com>
2023-01-05 00:26:52 +0100

    div only

--- assets/js/mode.js
+++ assets/js/mode.js
@@ -13,7 +13,6 @@ function switchToLightMode() {
   // document.getElementsByTagName("footer").classList.remove('night-mode');
   // document.getElementsByTagName("section").classList.remove('night-mode');
   // document.getElementsByTagName("a").classList.remove('night-mode');
-
   document.body.classList.add('light-mode');
   // divs.classList.add('light-mode')
   // document.getElementsByTagName("h3").classList.add('light-mode');
@@ -23,7 +22,6 @@ function switchToLightMode() {
   // document.getElementsByTagName("footer").classList.add('light-mode');
   // document.getElementsByTagName("section").classList.add('light-mode');
   // document.getElementsByTagName("a").classList.add('light-mode');
-  
   // modeIndicator.innerHTML = 'Light Mode';
   modeSwitch.checked = false;
   localStorage.setItem('mode', 'light');
@@ -33,7 +31,6 @@ function switchToNightMode() {
   document.body.classList.remove('light-mode');
   // var divs = document.getElementsByTagName("div");
   // divs.classList.remove('light-mode');
-
   // document.getElementsByTagName("h3").classList.remove('light-mode');
   // document.getElementsByTagName("h4").classList.remove('light-mode');
   // document.getElementsByTagName("p").classList.remove('light-mode');
@@ -41,7 +38,6 @@ function switchToNightMode() {
   // document.getElementsByTagName("footer").classList.remove('light-mode');
   // document.getElementsByTagName("section").classList.remove('light-mode');
   // document.getElementsByTagName("a").classList.remove('light-mode');
-
   document.body.classList.add('night-mode');
   // divs.classList.add('night-mode')
   // document.getElementsByTagName("h3").classList.add('night-mode');
